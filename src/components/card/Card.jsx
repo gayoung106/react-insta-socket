dfb52b248281a06ef6d35230960889aa72425133
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-10-18 09:05:30 +0900

    outline completed

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import "./card.css";
+import Heart from "../../img/heart.png";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Info from "../../img/info.png";
+import HeartFilled from "../../img/heartfilled.png";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const handleNotification = () => {
+    setLiked(true);
+  };
+
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img src={HeartFilled} alt="" className="cardIcon" />
+        ) : (
+          <img
+            src={Heart}
+            alt=""
+            className="cardIcon"
+            onClick={handleNotification}
+          />
+        )}
+
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./navbar.css";
+import Notification from "../../img/notification.png";
+import Message from "../../img/message.png";
+import Settings from "../../img/settings.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Gayoung App</span>
+      <div className="icons">
+        <div className="icon">
+          <img src={Notification} className="IconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Message} className="IconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon">
+          <img src={Settings} className="IconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
